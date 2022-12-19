fe4bdd8f7bde904449214b9d010cbabf2390c3e9
yooyongkuk <ykyoo0110@nate.com>
2022-12-19 17:40:20 +0900

    1219

--- js/sub.js
+++ js/sub.js
@@ -13,3 +13,42 @@
             $('.b').hide();
             }
         });
+
+    // $('.prev').click(function(){
+    //     $('.slide li:last').prependTo('.slide');
+    //     $('.slide').css('margin-left',-1140);
+    //     $('.slide').stop().animate({marginLeft:0},800);
+    //     });
+
+    // $('.next').click(function(){
+    //     $('.slide').stop().animate({marginLeft:-1140},800, function(){
+    //     $('.slide li:first').appendTo('.slide');
+    //     $('.slide').css({marginLeft:0});
+    //     });
+    //     });
+$(document).ready(function () {
+    function prev() {
+        $('.slide li:last').prependTo('.slide');
+        $('.slide').css('margin-left', '-100%');
+        $('.slide').stop().animate({ marginLeft: 0 }, 800);
+    }
+
+    function next() {
+        $('.slide').stop().animate({ marginLeft: '-100%' }, 800, function () {
+            $('.slide li:first').appendTo('.slide');
+            $('.slide').css({ marginLeft: 0 });
+        });
+    }
+
+    setInterval(next, 3000);
+
+    $('.prev').click(function () {
+        prev();
+    });
+    $('.next').click(function () {
+        next();
+    });
+});
+
+    
+        
