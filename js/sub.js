763d2f582a0efded844cb985879247d1c664c62f
yooyongkuk <ykyoo0110@nate.com>
2023-01-03 09:12:06 +0900

    0103_1

--- js/sub.js
+++ js/sub.js
@@ -1,7 +1,8 @@
 
-     $(function(){
+    //네비게이션
+    $(function(){
         $(".a").click(function(){
-            $(".b").slideToggle();
+            $(".b ul").slideToggle();
      });
     });
 
@@ -14,41 +15,56 @@
             }
         });
 
-    // $('.prev').click(function(){
-    //     $('.slide li:last').prependTo('.slide');
-    //     $('.slide').css('margin-left',-1140);
-    //     $('.slide').stop().animate({marginLeft:0},800);
-    //     });
+    //슬라이더
+    $(document).ready(function () {
+        function prev() {
+            $('.slide li:last').prependTo('.slide');
+            $('.slide').css('margin-left', '-100%');
+            $('.slide').stop().animate({ marginLeft: 0 }, 800);
+        }
 
-    // $('.next').click(function(){
-    //     $('.slide').stop().animate({marginLeft:-1140},800, function(){
-    //     $('.slide li:first').appendTo('.slide');
-    //     $('.slide').css({marginLeft:0});
-    //     });
-    //     });
-$(document).ready(function () {
-    function prev() {
-        $('.slide li:last').prependTo('.slide');
-        $('.slide').css('margin-left', '-100%');
-        $('.slide').stop().animate({ marginLeft: 0 }, 800);
-    }
+        function next() {
+            $('.slide').stop().animate({ marginLeft: '-100%' }, 800, function () {
+                $('.slide li:first').appendTo('.slide');
+                $('.slide').css({ marginLeft: 0 });
+            });
+        }
 
-    function next() {
-        $('.slide').stop().animate({ marginLeft: '-100%' }, 800, function () {
-            $('.slide li:first').appendTo('.slide');
-            $('.slide').css({ marginLeft: 0 });
+        setInterval(next, 3000);
+
+        $('.prev').click(function () {
+            prev();
         });
-    }
-
-    setInterval(next, 3000);
-
-    $('.prev').click(function () {
-        prev();
+        $('.next').click(function () {
+            next();
+        });
     });
-    $('.next').click(function () {
-        next();
+
+    //브로그슬라이드
+    $(document).ready(function() {
+        var totalNum = $(".vis > div").length 
+        var currentNum = 1;
+        $(".page_n > span:first").text(currentNum);
+        $(".page_n > span:last").text(totalNum);
+        $(".next_b").click(function() {
+            currentNum++;
+            if (currentNum > totalNum) {
+                currentNum = 1;
+            }
+            $(".vis div:first").insertAfter(".vis div:last");
+            $(".page_n > span:first").text(currentNum);
+        });
+        $(".prev_b").click(function() {
+            currentNum--;
+            if (currentNum < 1) {
+                currentNum = totalNum;
+            }
+            $(".vis div:last").insertBefore(".vis div:first");
+            $(".page_n > span:first").text(currentNum);
+        });
     });
-});
+
+    
 
     
         
--- js/sub_2.js
+++ js/sub_2.js
@@ -1,7 +1,7 @@
 
      $(function(){
         $(".a").click(function(){
-            $(".b").slideToggle();
+            $(".b ul").slideToggle();
      });
     });
 
@@ -13,6 +13,12 @@
             $('.b').hide();
             }
         });
+
+        // $(function(){
+        //     $(".b li").click(function(){
+        //         $(".b").slideToggle();
+        //  });
+        // });
 
     //코다마 토글-->
 
@@ -131,3 +137,58 @@
             kao.animate({width:"40px",opacity:"1"},"slow");   
         });
       });
+
+      //작은 슬라이드
+    $(document).ready(function() {
+        var totalNum = $(".vis > div").length 
+        var currentNum = 1;
+        $(".page_n > span:first").text(currentNum);
+        $(".page_n > span:last").text(totalNum);
+        $(".next_b").click(function() {
+            currentNum++;
+            if (currentNum > totalNum) {
+                currentNum = 1;
+            }
+            $(".vis div:first").insertAfter(".vis div:last");
+            $(".page_n > span:first").text(currentNum);
+        });
+        $(".prev_b").click(function() {
+            currentNum--;
+            if (currentNum < 1) {
+                currentNum = totalNum;
+            }
+            $(".vis div:last").insertBefore(".vis div:first");
+            $(".page_n > span:first").text(currentNum);
+        });
+    });
+
+    //스크롤 탑
+    $(window).ready(function() {
+        var isOn = false;
+        $(window).scroll(function() {
+           if($(this).scrollTop() > 100){
+               if(!isOn) {
+                $("#up").addClass("on");
+                   isOn = true;
+               }                   
+           }else{
+               if(isOn) {
+                   $("#up").removeClass("on");
+                   isOn = false;
+               }                   
+           }
+        });
+        $("#up a").click(function() {
+            $(window).scrollTop(0);                
+        });
+    });
+
+    //페이드인 슬라이드
+    $(document).ready(function() {
+        $("#control a").click(function() {
+            var num = Number($(this).text()) - 1;                 
+            $(this).addClass("on").siblings().removeClass("on");
+            $("#pic > div").filter(":visible").stop(true).fadeOut(350).end().eq(num).stop(true).fadeIn(350);
+        });
+        $("#control a:first").addClass("on").add("#pic > div:first").show();
+    });
